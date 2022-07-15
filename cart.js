af4503d67370a23261cdd34cd88e772a2f746b1a
Juan Cruz Montanelli <juancruzmotanelli@gmail.com>
2022-07-15 16:10:12 -0300

    hice que el carrito de compra funcione

--- cart.js
+++ cart.js
@@ -18,6 +18,8 @@ class picture {
 }
 
 
+let total = document.getElementById('total');
+
 
 let contenedorProductos = document.getElementById('productos');
 const cartProducts = [];
@@ -45,7 +47,25 @@ function agregarProducto(producto) {
         if (id == data.id) {
             console.log(data);
             cartProducts.push(data); 
-            console.log(cartProducts);      
+            console.log(cartProducts); 
+            
+            empty.remove();
+            cartChild.innerHTML += `<div class=" card_block wow animate__animated animate__bounceInLeft">
+                                                <img src=${data.img}>
+                                                <h3 class="content">${data.name}</h3>        
+                                                <h5 class="content">ARS$ ${data.price}</h5>
+                                            </div>`    
+            
+            const totalBuy = cartProducts.reduce ((acc, the) => acc + the.price, 0)
+
+            console.log(totalBuy)
+
+            total.innerHTML = `<div class="flex_card" id="total">
+                                <h2>ARS$ ${totalBuy} </h2>
+                                </div>
+                                `
+
+
         }
     }
     Toastify({
@@ -57,30 +77,17 @@ function agregarProducto(producto) {
         }).showToast();
 
         
-        for (let producto of cartProducts) {
-            cartChild.innerHTML += `<div class=" card_block wow animate__animated animate__bounceInLeft">
-                                                <img src=${producto.img}>
-                                                <h3 class="content">${producto.name}</h3>        
-                                                <h5 class="content">ARS$ ${producto.price}</h5>
-                                                <button type="submit" onclick="agregarProducto(this)"id="${producto.id}">add to the cart</button>
-                                            </div>`
-        };
+    
         
 };
 
-let cart = document.getElementById('cart')
 
-function addCart () {
-for (let producto of cartProducts) {
-    cart.innerHTML += `<div class=" card_block wow animate__animated animate__bounceInLeft">
-                                        <img src=${producto.img}>
-                                        <h3 class="content">${producto.name}</h3>        
-                                        <h5 class="content">ARS$ ${producto.price}</h5>
-                                        <button type="submit" onclick="agregarProducto(this)"id="${producto.id}">add to the cart</button>
-                                    </div>`
-};
-}
-addCart()
+
+
+
+
+
+
 
 
 
@@ -104,24 +111,14 @@ const cartPrice = [];
 
 let empty = document.getElementById('empty');
 
-let add1 = document.getElementById('add_1'); 
 
-let emptyCart = document.createElement('h2');
-
-
-emptyCart.innerText = 'The cart is empty'
-
-cartProducts.length === 0 && empty.appendChild(emptyCart);
+cartProducts.length === 0 && (empty.innerHTML = '<h2>The cart is empty</h2>');
 
 
 
-let total = document.getElementById('total');
 
-let totalContainer = document.createElement('h2');
 
-totalContainer.innerText = `ARS$ ${plus(...cartPrice)}`
 
-total.append(totalContainer);
 
 
 /*
@@ -181,7 +178,8 @@ lettry = document.getElementById('try');
 buy.addEventListener('click', (event) =>   {
     event.preventDefault();
     
-    
+    total.remove(); 
+    empty.remove()
     cartChild.innerHTML = '<h2>Buy competed!</h2>'
     Toastify({
 
